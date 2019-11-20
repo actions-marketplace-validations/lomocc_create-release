9b939856d128ee5aef18dbcca9a0c8282dc6a302
Vincent <const.lomo@gmail.com>
2019-11-20 15:14:04 +0800

    fix: catch release exist

--- src/create-release.js
+++ src/create-release.js
@@ -1,5 +1,5 @@
-const core = require('@actions/core');
-const { GitHub, context } = require('@actions/github');
+const core = require("@actions/core");
+const { GitHub, context } = require("@actions/github");
 
 async function run() {
   try {
@@ -10,27 +10,43 @@ async function run() {
     const { owner, repo } = context.repo;
 
     // Get the inputs from the workflow file: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
-    const tagName = core.getInput('tag_name', { required: true });
+    const tagName = core.getInput("tag_name", { required: true });
 
     // This removes the 'refs/tags' portion of the string, i.e. from 'refs/tags/v1.10.15' to 'v1.10.15'
-    const tag = tagName.replace('refs/tags/', '');
-    const releaseName = core.getInput('release_name', { required: true }).replace('refs/tags/', '');
-    const body = core.getInput('body', { required: false });
-    const draft = core.getInput('draft', { required: false }) === 'true';
-    const prerelease = core.getInput('prerelease', { required: false }) === 'true';
+    const tag = tagName.replace("refs/tags/", "");
+    const releaseName = core
+      .getInput("release_name", { required: true })
+      .replace("refs/tags/", "");
+    const body = core.getInput("body", { required: false });
+    const draft = core.getInput("draft", { required: false }) === "true";
+    const prerelease =
+      core.getInput("prerelease", { required: false }) === "true";
 
-    // Create a release
-    // API Documentation: https://developer.github.com/v3/repos/releases/#create-a-release
-    // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-create-release
-    const createReleaseResponse = await github.repos.createRelease({
-      owner,
-      repo,
-      tag_name: tag,
-      name: releaseName,
-      body,
-      draft,
-      prerelease
-    });
+    let createReleaseResponse;
+    try {
+      createReleaseResponse = await octokit.repos.getReleaseByTag({
+        owner,
+        repo,
+        tag
+      });
+    } catch (error) {
+      if (error.status === 404) {
+        // Create a release
+        // API Documentation: https://developer.github.com/v3/repos/releases/#create-a-release
+        // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-create-release
+        createReleaseResponse = await github.repos.createRelease({
+          owner,
+          repo,
+          tag_name: tag,
+          name: releaseName,
+          body,
+          draft,
+          prerelease
+        });
+      } else {
+        core.setFailed(error.message);
+      }
+    }
 
     // Get the ID, html_url, and upload URL for the created Release from the response
     const {
@@ -38,9 +54,9 @@ async function run() {
     } = createReleaseResponse;
 
     // Set the output variables for use by other actions: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
-    core.setOutput('id', releaseId);
-    core.setOutput('html_url', htmlUrl);
-    core.setOutput('upload_url', uploadUrl);
+    core.setOutput("id", releaseId);
+    core.setOutput("html_url", htmlUrl);
+    core.setOutput("upload_url", uploadUrl);
   } catch (error) {
     core.setFailed(error.message);
   }
